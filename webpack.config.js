7d445a99b67e09835b7d314628c46cefd810fb09
izwushi <115918924+izwushi@users.noreply.github.com>
2023-02-12 18:58:22 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require ('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { loader } = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/javascripts/main.js',
@@ -34,6 +35,20 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader:'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
@@ -41,7 +56,12 @@ module.exports = {
             filename: './stylesheets/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
+            template: './src/templates/index.pug',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
     ],
